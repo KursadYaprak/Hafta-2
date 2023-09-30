924308312d6dc4eb4f55804253ba372d1cd797a4
Kürşad Yaprak <kursadyaprak80@hotmail.com>
2023-09-30 23:12:21 +0300

    Dizideki Elemanları Sıralama

--- DizideElemanSiralama/src/Main.java
+++ DizideElemanSiralama/src/Main.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int i = 1, eleman, num = 0;
+
+        System.out.print("Dizinin boyutu n : ");
+        int n = inp.nextInt();
+
+        int[] numbers = new int[n];
+
+        System.out.println("Dizinin elemanlarını giriniz :");
+
+        while (i <= n) {
+                System.out.print(i + ". Elemanı : ");
+                eleman = inp.nextInt();
+                i++;
+                numbers[num] = eleman;
+                num++;
+        }
+        System.out.print("Sıralama : ");
+        System.out.println(Arrays.toString(numbers));
+    }
 }
